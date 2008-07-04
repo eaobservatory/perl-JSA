a9802b86462d3320e74bafb71ea28ddc7c0c1ad2
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2008-07-04 08:50:35 +0000

    - Change `` to qx// so that `` in light weight font don't get lost.
    svn path=/trunk/perlmods/; revision=15716

--- lib/JSA/EnterData.pm
+++ lib/JSA/EnterData.pm
@@ -1387,7 +1387,7 @@ sub calc_radec {
   my %result;
   for my $k ( qw/ FTL FBR FTR FBL /) {
 
-    my $res = `/star/bin/kappa/parget $k makecube`;
+    my $res = qx{ /star/bin/kappa/parget $k makecube };
     $res =~ s/^\s+//;
     $res =~ s/\s+$//;
     $result{$k} = [ map {Astro::Coords::Angle->new( $_, units => 'rad') } split(/\s+/,$res) ];
@@ -1414,7 +1414,7 @@ sub calc_radec {
 
     for my $k (qw/ REFLON REFLAT / ) {
 
-      my $res = `/star/bin/kappa/parget $k makecube`;
+      my $res = qx{ /star/bin/kappa/parget $k makecube };
       chomp($res);
       $result{$k} = $res;
     }
