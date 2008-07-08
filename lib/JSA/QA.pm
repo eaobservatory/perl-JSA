2e268a125ef2542c844729a79b4eb1902e1093da
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2008-07-08 02:19:45 +0000

    split off JSA::QA::Result into a separate file
    svn path=/trunk/perlmods/; revision=15732

--- lib/JSA/QA.pm
+++ lib/JSA/QA.pm
@@ -23,6 +23,8 @@ use warnings;
 use warnings::register;
 use Carp;
 
+use JSA::QA::Result;
+
 use vars qw/ $VERSION $DEBUG /;
 
 use Exporter 'import';
@@ -359,72 +361,7 @@ sub _min_max_mean {
 
 
 
-package JSA::QA::Result;
 
-sub new {
-  my $proto = shift;
-  my $class = ref( $proto ) || $proto;
-
-  my $result = bless { 'PASS' => 1,
-                       'BAD_RECEPTORS' => [],
-                       'FAIL_REASONS' => [],
-                     }, $class;
-
-  $result->_configure( @_ ) if @_;
-
-  return $result;
-}
-
-sub bad_receptors {
-  my $self = shift;
-  if( @_ ) { $self->{BAD_RECEPTORS} = shift; }
-  return $self->{BAD_RECEPTORS};
-}
-
-sub fail_reasons {
-  my $self = shift;
-  if( @_ ) { $self->{FAIL_REASONS} = shift; }
-  return $self->{FAIL_REASONS};
-}
-
-sub pass {
-  my $self = shift;
-  if( @_ ) { $self->{PASS} = shift; }
-  return $self->{PASS};
-}
-
-sub add_bad_receptor {
-  my $self = shift;
-  my $receptor = shift;
-
-  return if ! defined $receptor;
-
-  my $bad_receptors = $self->bad_receptors;
-  push @$bad_receptors, $receptor;
-  $self->bad_receptors( $bad_receptors );
-}
-
-sub add_fail_reason {
-  my $self = shift;
-  my $reason = shift;
-
-  return if ! defined $reason;
-
-  my $reasons = $self->fail_reasons;
-  push @$reasons, $reason;
-  $self->fail_reasons( $reasons );
-}
-
-sub _configure {
-  my $self = shift;
-  my %args = @_;
-
-  foreach my $key ( keys %args ) {
-    if( $self->can( $key ) ) {
-      $self->$key( $args{$key} );
-    }
-  }
-}
 
 package JSA::QA::Telescope;
 
---
+++ lib/JSA/QA/Result.pm
@@ -0,0 +1,77 @@
+package JSA::QA::Result;
+
+use 5.006;
+use strict;
+use warnings;
+use Carp;
+
+use vars qw/ $VERSION $DEBUG /;
+
+use Exporter 'import';
+
+sub new {
+  my $proto = shift;
+  my $class = ref( $proto ) || $proto;
+
+  my $result = bless { 'PASS' => 1,
+                       'BAD_RECEPTORS' => [],
+                       'FAIL_REASONS' => [],
+                     }, $class;
+
+  $result->_configure( @_ ) if @_;
+
+  return $result;
+}
+
+sub bad_receptors {
+  my $self = shift;
+  if( @_ ) { $self->{BAD_RECEPTORS} = shift; }
+  return $self->{BAD_RECEPTORS};
+}
+
+sub fail_reasons {
+  my $self = shift;
+  if( @_ ) { $self->{FAIL_REASONS} = shift; }
+  return $self->{FAIL_REASONS};
+}
+
+sub pass {
+  my $self = shift;
+  if( @_ ) { $self->{PASS} = shift; }
+  return $self->{PASS};
+}
+
+sub add_bad_receptor {
+  my $self = shift;
+  my $receptor = shift;
+
+  return if ! defined $receptor;
+
+  my $bad_receptors = $self->bad_receptors;
+  push @$bad_receptors, $receptor;
+  $self->bad_receptors( $bad_receptors );
+}
+
+sub add_fail_reason {
+  my $self = shift;
+  my $reason = shift;
+
+  return if ! defined $reason;
+
+  my $reasons = $self->fail_reasons;
+  push @$reasons, $reason;
+  $self->fail_reasons( $reasons );
+}
+
+sub _configure {
+  my $self = shift;
+  my %args = @_;
+
+  foreach my $key ( keys %args ) {
+    if( $self->can( $key ) ) {
+      $self->$key( $args{$key} );
+    }
+  }
+}
+
+1;
