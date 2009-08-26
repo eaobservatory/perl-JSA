f1f7d2e221f477eae5f4646543b8310fb1669997
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2009-08-25 16:26:13 -1000

    Add instrument parent class.  Currently it has debug() as the only method.

---
+++ lib/JSA/EnterData/Instrument.pm
@@ -0,0 +1,49 @@
+package JSA::EnterData::Instrument;
+
+use strict;
+use warnings;
+
+our $DEBUG;
+
+my %_default =
+ ( 'debug' => 0,
+ );
+
+sub new {
+
+  my ( $class, %args ) = @_;
+
+  my $obj = bless {}, $class;
+
+  for my $k ( keys %_default ) {
+
+    if ( my $sub = $obj->can( $k ) ) {
+
+      $obj->$sub( exists $args{ $k } ? $args{ $k } : $_default{ $k } );
+    }
+  }
+
+  return $obj;
+}
+
+# It is in addition to direct access to
+# $JSA::EnterData::Instrument::DEBUG.
+sub debug {
+
+  my $self = shift @_;
+
+  return $DEBUG
+    unless scalar @_;
+
+  $DEBUG = !! $_[0];
+  return;
+}
+
+1;
+
+=pod
+
+
+=cut
+
+
