e48d7bb22de4829595dbb215d9e7ba8f418041c7
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2012-01-13 16:47:44 -1000

    A: Swicth to /stardev as "bounds retrieval code ... [has] ... rounding bug" ...
    ... in /star on summit; see
    <CA+G92RdmkLP6G-ba3eEKuGjKvnTZr2SKoqWrdGtQKD99wi-KOw@mail.gmail.com> mail by
    Tim J.

--- lib/JSA/EnterData/ACSIS.pm
+++ lib/JSA/EnterData/ACSIS.pm
@@ -71,7 +71,7 @@ sub get_bound_check_command {
 
   # Turn off autogrid; only rotate raster maps. Just need bounds.
   return
-    ( '/star/bin/smurf/makecube',
+    ( '/stardev/bin/smurf/makecube',
       "in=^$fh",
       'system=ICRS',
       'out=!',
