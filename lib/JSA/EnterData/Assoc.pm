333959c3e4c7dec7f3bad31435d6462bd7144021
A Agarwal <a.agarwal@jach.hawaii.edu>
2014-05-13 05:06:04 -1000

    A: Use Graham's jsa_pub_asn_id() instead, after long deliberation for ACSIS.
    Change to be sub based for having only 1 sub. Tidy up.

--- lib/JSA/EnterData/Assoc.pm
+++ lib/JSA/EnterData/Assoc.pm
@@ -1,8 +1,13 @@
 package JSA::EnterData::Assoc;
 
-use strict; use warnings FATAL => 'all';
+use strict; use warnings;
 
-our $VERSION = '0.00';
+our $VERSION = '0.01';
+
+use Exporter 'import';
+@EXPORT_OK = qw( get_asn_id );
+
+use Log::Log4perl;
 
 use JSA::Error       qw[ :try ];
 use JSA::Headers     (); #get_orac_instrument
@@ -12,18 +17,20 @@ my %loaded;
 
 sub get_asn_id {
 
-  my ( $class, $obs ) = @_;
+  my ( $obs ) = @_;
 
   my $orac_inst = JSA::Headers::get_orac_instrument( $obs->fits() );
   my ( $frame_class, $group, $cal, $inst ) =
     ORAC::Inst::Defn::orac_determine_inst_classes( $orac_inst );
+
+  my $log = Log::Log4perl->get_logger( '' );
 
   unless ( exists $loaded{ $frame_class } ) {
 
     ( my $load = $frame_class ) =~ s{::}{/}g;
     $load .= q[.pm];
     eval { require $load; 1; }
-      or die "Could not load '$frame_class' class: ", $@ ;
+      or $log->logcroak( "Could not load '$frame_class' class: ", $@  );
   }
 
   my $headers = $obs->hdrhash();
@@ -36,24 +43,22 @@ sub get_asn_id {
     ;
 
   my %out;
-
   for my $subsys ( $obs->subsystems() ) {
 
     my @idss = $subsys->obsidss();
     my $idss = $idss[0];
     unless ( $idss ) {
 
-      Carp::carp( 'Cannot find OBDISS for an observation.' );
+      $log->warn( 'Cannot find OBDISS for an observation.' );
       next;
     }
 
     my $frame = $frame_class->new();
     $frame->configure( [ $subsys->filename() ] );
-    $frame->findgroup();
 
-    push @{ $out{ $idss } } , $frame->asn_id();
+    my $id =  $frame->jsa_pub_asn_id() or next;
+    push @{ $out{ $idss } }, $id;
   }
-
   return %out;
 }
 
@@ -69,23 +74,57 @@ JSA::EnterData::Assoc - Find asn_id.
 
 =head1 SYNOPSIS
 
+To get ASN ID for ACSIS(HARP) or SCUBA2 instrument ...
+
+  use  JSA::EnterData::Assoc;
+  $asn_list = JSA::EnterData::Assoc::get_asn_id( $obs );
 
 =head1  DESCRIPTION
 
+Currently it is a function based module. Nothing is exported by default.
 
-
-=head2  METHODS|FUNCTIONS
+=head2 Functions
 
 =over 2
 
-=item *
+=item B<get_asn_id>
 
+Given a L<OMP::Info::Obs> object, returns a hash of C<obsid_subsysnr> as keys
+asn ASN ID as array reference values. It can be imported in your code.
 
-=item *
-
+ $list = get_asn_id( $obs );
 
 =back
 
-=cut
+=head2 See Also
 
+=over 2
+
+=item L<OMP::Info::Obs>
+
+=item I<ORAC-DR> Perl modules
+
+=back
+
+=head1 COPYRIGHT, LICENSE
+
+Copyright (C) 2014, Science and Technology Facilities Council.
+All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful,but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA  02111-1307,
+USA
+
+=cut
 
