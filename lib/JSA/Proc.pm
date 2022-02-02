83857e315c6da6e64d9e36faa2de4d7e3ec53955
Graham Bell <g.bell@eaobservatory.org>
2022-02-02 11:29:54 -1000

    JSA::Proc: extract function to open Taco connexion

--- lib/JSA/Proc.pm
+++ lib/JSA/Proc.pm
@@ -79,25 +79,98 @@ B<Note:> only applies when used in conjunction "allow_update"
 
 =cut
 
+sub add_jsa_proc_jobs {
+    my $groups = shift;
+    my $mode = shift;
+    my $priority = shift;
+    my $dry_run = shift;
+    my %options = @_;
+
+    log_message("\nBeginning to add jobs to local jsa_proc system.\n");
+
+    my ($taco, $jsa_proc_db);
+    unless ($dry_run) {
+        ($taco, $jsa_proc_db) = _get_taco_jsa_proc_db(\%options);
+    }
+    elsif ($dry_run) {
+        log_message("Skipping connection to jsa_proc. [DRY RUN MODE]\n");
+    }
+
+    my $location = 'JAC';
+    log_message("\nProcessing jobs for location: $location\n");
+
+    while (my ($tag, $group) = each %$groups) {
+        log_message("\nProcessing job with tag: $tag\n");
+
+        # Make list of plain file names with the extensions removed.
+        my @files = map {s/\.sdf$//; $_} @{$group->{'files'}};
+
+
+        # Job submission mode.
+
+        # Prepare arguments for the jsa_proc database add_job method.
+        my @param = ();
+        push @param, '--recpars', $group->{'recpars'}
+            if exists $group->{'recpars'};
+        push @param, $group->{'drparams'}
+            if exists $group->{'drparams'};
+
+        my %args = (
+            tag             => $tag,
+            location        => $location,
+            mode            => $group->{'mode'} // $mode,
+            parameters      => join(' ', @param),
+            input_file_names=> \@files,
+            priority        => $priority,
+            obsidss         => \@{$group->{'obsid_subsysnr_list'}},
+            task            => $group->{'task'},
+            );
+
+        unless ($dry_run) {
+            # In non-dry run mode, try calling the add_job method.
+
+            log_message("Adding job to jsa_proc database.\n");
+            eval {
+                my $job_id;
+                unless ($options{'allow_update'}) {
+                    $job_id = $jsa_proc_db->call_method(
+                        'add_job', kwargs => \%args);
+                }
+                else {
+                    $args{'db'} = $jsa_proc_db;
+                    $args{'dry_run'} = $options{'jsa_proc_dry_run'};
+                    $job_id = $taco->call_function(
+                        'add_upd_del_job', kwargs => \%args);
+                }
+                log_message("Job added/updated with ID: $job_id\n");
+            };
+            if ($@) {
+                log_message("ERROR: failed to add/update job:\n$@\n");
+            }
+        }
+        else {
+            # In dry run mode, print the arguments we would have given.
+
+            log_message("Would have added the job: [DRY RUN MODE]\n");
+            log_message(Data::Dumper->Dump([\%args], [qw/args/]));
+        }
+
+    }
+
+    log_message("\nDone adding jobs to local jsa_proc system.\n")
+}
+
 {
     # Taco client for the connection to Python, to be constructed when
     # required.
     my $taco = undef;
     my $jsa_proc_db = undef;
 
-    sub add_jsa_proc_jobs {
-        my $groups = shift;
-        my $mode = shift;
-        my $priority = shift;
-        my $dry_run = shift;
-        my %options = @_;
-
-        log_message("\nBeginning to add jobs to local jsa_proc system.\n");
+    sub _get_taco_jsa_proc_db {
+        my $options = shift;
 
         # Create Taco connection to Python unless we already have one.
-        # Submission mode dry run does not require a connection, but
-        # info adding dry run does.
-        unless (($dry_run) or defined $jsa_proc_db) {
+        unless (defined $jsa_proc_db) {
             log_message("Opening connection to jsa_proc.\n");
             $taco = new Alien::Taco(lang => 'python');
             $taco->import_module('jsa_proc.config', args => ['get_database']);
@@ -105,84 +178,18 @@ B<Note:> only applies when used in conjunction "allow_update"
                                  args => ['add_upd_del_job']);
             $jsa_proc_db = $taco->call_function('get_database');
 
-            if ($options{'jsa_proc_logging'}) {
+            if ($options->{'jsa_proc_logging'}) {
                 $taco->import_module('logging');
                 $taco->call_function(
                     'logging.basicConfig', kwargs => {level =>
                         $taco->get_value(
-                            'logging.' . uc($options{'jsa_proc_logging'}))});
+                            'logging.' . uc($options->{'jsa_proc_logging'}))});
             }
         }
-        elsif ($dry_run) {
-            log_message("Skipping connection to jsa_proc. [DRY RUN MODE]\n");
-        }
 
-        my $location = 'JAC';
-        log_message("\nProcessing jobs for location: $location\n");
-
-        while (my ($tag, $group) = each %$groups) {
-            log_message("\nProcessing job with tag: $tag\n");
-
-            # Make list of plain file names with the extensions removed.
-            my @files = map {s/\.sdf$//; $_} @{$group->{'files'}};
-
-
-            # Job submission mode.
-
-            # Prepare arguments for the jsa_proc database add_job method.
-            my @param = ();
-            push @param, '--recpars', $group->{'recpars'}
-                if exists $group->{'recpars'};
-            push @param, $group->{'drparams'}
-                if exists $group->{'drparams'};
-
-            my %args = (
-                tag             => $tag,
-                location        => $location,
-                mode            => $group->{'mode'} // $mode,
-                parameters      => join(' ', @param),
-                input_file_names=> \@files,
-                priority        => $priority,
-                obsidss         => \@{$group->{'obsid_subsysnr_list'}},
-                task            => $group->{'task'},
-                );
-
-            unless ($dry_run) {
-                # In non-dry run mode, try calling the add_job method.
-
-                log_message("Adding job to jsa_proc database.\n");
-                eval {
-                    my $job_id;
-                    unless ($options{'allow_update'}) {
-                        $job_id = $jsa_proc_db->call_method(
-                            'add_job', kwargs => \%args);
-                    }
-                    else {
-                        $args{'db'} = $jsa_proc_db;
-                        $args{'dry_run'} = $options{'jsa_proc_dry_run'};
-                        $job_id = $taco->call_function(
-                            'add_upd_del_job', kwargs => \%args);
-                    }
-                    log_message("Job added/updated with ID: $job_id\n");
-                };
-                if ($@) {
-                    log_message("ERROR: failed to add/update job:\n$@\n");
-                }
-            }
-            else {
-                # In dry run mode, print the arguments we would have given.
-
-                log_message("Would have added the job: [DRY RUN MODE]\n");
-                log_message(Data::Dumper->Dump([\%args], [qw/args/]));
-            }
-
-        }
-
-        log_message("\nDone adding jobs to local jsa_proc system.\n")
+        return ($taco, $jsa_proc_db);
     }
 }
-
-
 
 =back
 
